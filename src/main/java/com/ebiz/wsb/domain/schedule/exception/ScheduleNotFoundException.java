fbb395c3a7eb44a20b39f01fe0f912c3edd189df
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-02-18 21:56:32 +0900

    chore: 스케쥴 기능 삭제

--- src/main/java/com/ebiz/wsb/domain/schedule/exception/ScheduleNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ebiz.wsb.domain.schedule.exception;
-
-public class ScheduleNotFoundException extends RuntimeException{
-    public ScheduleNotFoundException(String message){
-        super(message);
-    }
-}
