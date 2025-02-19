5d84bf7b7929da11f949e8429355b8f378417eda
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-02-19 13:17:40 +0900

    refactor: GroupNotAccessException 불필요한 예외 삭제

--- src/main/java/com/ebiz/wsb/domain/group/exception/GroupNotAccessException.java
+++
@@ -1,8 +0,0 @@
-package com.ebiz.wsb.domain.group.exception;
-
-public class GroupNotAccessException extends RuntimeException{
-
-    public GroupNotAccessException(String message) {
-        super(message);
-    }
-}
