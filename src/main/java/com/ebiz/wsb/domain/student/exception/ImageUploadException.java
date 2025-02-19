4701f2e318c192ebf2d5262a6aad47e9abe181ee
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-02-19 11:44:53 +0900

    refactor: 불필요한 GroupStudentAssignRequest.java, ImageUploadException.java, StudentCreateRequestDTO.java 삭제

--- src/main/java/com/ebiz/wsb/domain/student/exception/ImageUploadException.java
+++
@@ -1,7 +0,0 @@
-package com.ebiz.wsb.domain.student.exception;
-
-public class ImageUploadException extends RuntimeException{
-    public ImageUploadException(String message, Throwable cause){
-        super(message, cause);
-    }
-}
