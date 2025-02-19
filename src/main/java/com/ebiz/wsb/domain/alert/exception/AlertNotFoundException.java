a92ac61317f350aa8fd1868691ae83f662979662
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-02-19 19:33:16 +0900

    refactor: 불필요한 AlertNotFoundException 예의 삭제

--- src/main/java/com/ebiz/wsb/domain/alert/exception/AlertNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ebiz.wsb.domain.alert.exception;
-
-public class AlertNotFoundException extends RuntimeException{
-    public AlertNotFoundException(String message) {
-        super(message);
-    }
-}
