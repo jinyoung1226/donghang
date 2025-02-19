20f5e96c92a215174f3ad632f09ec1dd76f99e30
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-02-19 11:59:30 +0900

    refactor: 테스트 코드 삭제

--- src/main/java/com/ebiz/wsb/domain/location/exception/InvalidLocationDataException.java
+++
@@ -1,7 +0,0 @@
-package com.ebiz.wsb.domain.location.exception;
-
-public class InvalidLocationDataException extends RuntimeException{
-    public InvalidLocationDataException(String message){
-        super(message);
-    }
-}
