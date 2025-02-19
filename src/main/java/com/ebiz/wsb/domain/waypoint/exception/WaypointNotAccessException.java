ab9bbf29b14cea4b832a1356fdf70e997f6ecea8
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-02-19 12:44:20 +0900

    refactor: parentExceptionHandler, WaypointNotAccessException 불필요한 예외 핸들러 삭제

--- src/main/java/com/ebiz/wsb/domain/waypoint/exception/WaypointNotAccessException.java
+++
@@ -1,7 +0,0 @@
-package com.ebiz.wsb.domain.waypoint.exception;
-
-public class WaypointNotAccessException extends RuntimeException{
-    public WaypointNotAccessException(String message) {
-        super(message);
-    }
-}
