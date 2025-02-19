113b6796510c4cb891e95e86f4547bea654956d8
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-02-19 10:34:28 +0900

    refactor: 사용 안하는 FileUploadException.java, GuardianNotAccessException.java 삭제

--- src/main/java/com/ebiz/wsb/domain/guardian/exception/FileUploadException.java
+++
@@ -1,7 +0,0 @@
-package com.ebiz.wsb.domain.guardian.exception;
-
-public class FileUploadException extends RuntimeException{
-    public FileUploadException(String message, Throwable cause){
-        super(message, cause);
-    }
-}
--- src/main/java/com/ebiz/wsb/domain/guardian/exception/GuardianNotAccessException.java
+++
@@ -1,7 +0,0 @@
-package com.ebiz.wsb.domain.guardian.exception;
-
-public class GuardianNotAccessException extends RuntimeException{
-    public GuardianNotAccessException(String message){
-        super(message);
-    }
-}
