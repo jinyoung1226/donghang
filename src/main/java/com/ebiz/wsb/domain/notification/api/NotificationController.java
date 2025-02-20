8cf093259502565a660c9402909109421f407ba0
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-02-20 12:14:59 +0900

    refactor: Notification 스케쥴 공지사항 삭제

--- src/main/java/com/ebiz/wsb/domain/notification/api/NotificationController.java
+++ src/main/java/com/ebiz/wsb/domain/notification/api/NotificationController.java
@@ -3,15 +3,12 @@ package com.ebiz.wsb.domain.notification.api;
 
 import com.ebiz.wsb.domain.notification.application.PushNotificationService;
 import com.ebiz.wsb.domain.notification.dto.PushRequest;
-import com.ebiz.wsb.domain.notification.dto.PushType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,15 +25,5 @@ public class NotificationController {
         } catch (Exception e) {
             return ResponseEntity.status(500).body("푸시 알림 전송 실패: " + e.getMessage());
         }
-    }
-    @PostMapping("/send-group")
-    public ResponseEntity<String> sendGroupNotification(@RequestBody Map<String, Object> request) {
-        Long groupId = Long.valueOf(request.get("groupId").toString());
-        String title = (String) request.get("title");
-        String body = (String) request.get("body");
-        PushType pushType = PushType.valueOf((String) request.get("pushType"));
-
-        pushNotificationService.sendPushNotificationToGroup(groupId, title, body, pushType);
-        return ResponseEntity.ok("그룹에 푸시 알림이 성공적으로 전송되었습니다.");
     }
 }
