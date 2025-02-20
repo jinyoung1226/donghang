52b9a47cf61db5e9229b7e1073809eca4371675c
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-02-20 09:45:36 +0900

    refactor: 불필요한 StatusNotFoundException 예외 삭제

--- src/main/java/com/ebiz/wsb/domain/attendance/exception/StatusNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ebiz.wsb.domain.attendance.exception;
-
-public class StatusNotFoundException extends RuntimeException{
-    public StatusNotFoundException(String message){
-        super(message);
-    }
-}
