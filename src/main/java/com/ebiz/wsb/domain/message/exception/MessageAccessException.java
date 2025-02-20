14febdd50a53559e29e0838d97e8a79f3591f297
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-02-20 11:59:33 +0900

    refactor: 불필요한 MessageAccessException, ParentAccessException 예외 처리 삭제

--- src/main/java/com/ebiz/wsb/domain/message/exception/MessageAccessException.java
+++
@@ -1,9 +0,0 @@
-package com.ebiz.wsb.domain.message.exception;
-
-import com.ebiz.wsb.domain.message.entity.Message;
-
-public class MessageAccessException extends RuntimeException{
-    public MessageAccessException(String message){
-        super(message);
-    }
-}
--- src/main/java/com/ebiz/wsb/domain/parent/exception/ParentAccessException.java
+++
@@ -1,7 +0,0 @@
-package com.ebiz.wsb.domain.parent.exception;
-
-public class ParentAccessException extends RuntimeException{
-    public ParentAccessException(String message){
-        super(message);
-    }
-}
